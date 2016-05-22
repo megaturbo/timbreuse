e104696869270a30b8d311382e2a6b808f8934e7
roflolilolmao <quentin.jeanmonod@he-arc.ch>
2016-05-22 03:24:02 +0200

    nothing working

--- timbreuse.py
+++ timbreuse.py
@@ -2,6 +2,11 @@ from flask import Flask, \
 	render_template
 from flask_sqlalchemy import SQLAlchemy
 from config import DevelopmentConfig as Config
+from flask.ext.login import LoginManager
+
+from flask import Flask, session, request, flash, url_for, redirect, render_template, abort, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -9,10 +14,52 @@ db = SQLAlchemy(app)
 
 from models import *
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    user = User(request.form['username'], request.form['password'])
+    db.session.add(user)
+    db.session.commit()
+    flash('User successfully registered')
+    return redirect(url_for('login'))
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    return redirect(url_for('index'))
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    username = request.form['username']
+    password = request.form['password']
+    registered_user = User.query.filter_by(username=username, password=password).first()
+    if registered_user is None:
+        flash('Username or Password is invalid' , 'error')
+        return redirect(url_for('login'))
+    login_user(registered_user)
+    flash('Logged in successfully')
+    return redirect(request.args.get('next') or url_for('index'))
 
 
 if __name__ == '__main__':
